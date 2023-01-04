323d69a78349ab9fc36a6fe0bda286a2add2e0d2
mrGreedySoap <107418049+mrGreedySoap@users.noreply.github.com>
2023-01-04 17:10:40 +0700

    feat | отправка json словаря

--- produce_message.py
+++ produce_message.py
@@ -1,29 +1,27 @@
 import pika, sys, os
+import json
 
 
-
-def string_message():
+def json_message():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='str')
+    channel.queue_declare(queue='json')
 
     def callback(ch, method, properties, body):
         print(f" [x] Received {body}")
-        print(f"body to str -> {body.decode('utf-8')},{type(str(body))}")
+        message = json.loads(body.decode('utf-8'))
+        print(f"message, type: {message}, {type(message)}")
 
-    channel.basic_consume(queue='str', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='json', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
 
-def json_message():
-
-
 if __name__ == '__main__':
     try:
-        string_message()
+        json_message()
     except KeyboardInterrupt:
         print('Interrupted')
         try:
--- receive_message.py
+++ receive_message.py
@@ -1,11 +1,20 @@
 import pika
+import json
 
-message = str(0x8544)
+json_dict = {"Date": "2016-05-21T21:35:40Z",
+             "CreationDate": "2012-05-05",
+             "LogoType": "png",
+             "Ref": 164611595,
+             "Classe": ["Email addresses", "Passwords"],
+             "Link": "http://some_link.com"}
+
+message = json.dumps(json_dict)
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='str')
+channel.queue_declare(queue='json')
 channel.basic_publish(exchange='',
-                      routing_key='str',
+                      routing_key='json',
                       body=message)
 print(f" [x] Sent '{message}'")
 connection.close()
+
