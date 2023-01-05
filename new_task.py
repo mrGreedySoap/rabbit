9f5a7f07997b6958754c4754e9228c6d1c0b204f
mrGreedySoap <107418049+mrGreedySoap@users.noreply.github.com>
2023-01-05 17:54:54 +0700

    feat | Сохранение сообщений на сервере

--- new_task.py
+++ new_task.py
@@ -4,13 +4,16 @@ import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='json')
+channel.queue_declare(queue='task_queue', durable=True)
 
 message = '...' + ''.join(sys.argv[1:])
 print(' '.join(sys.argv[1:]))
 channel.basic_publish(exchange='',
-                      routing_key='json',
-                      body=message)
+                      routing_key="task_queue",
+                      body=message,
+                      properties=pika.BasicProperties(
+                      delivery_mode = pika.spec.PERSISTENT_DELIVERY_MODE
+                      ))
 print(" [x] Sent %r" % message)
 
 connection.close()
--- worker.py
+++ worker.py
@@ -6,8 +6,7 @@ import json
 def json_message():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
-
-    channel.queue_declare(queue='json')
+    channel.queue_declare(queue='task_queue', durable=True)
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body.decode())
@@ -15,7 +14,7 @@ def json_message():
         print(" [x] Done")
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    channel.basic_consume(queue='json', on_message_callback=callback)
+    channel.basic_consume(queue='task_queue', on_message_callback=callback)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
