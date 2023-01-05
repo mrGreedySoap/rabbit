797b55b2e4bcb8ea08e92361d68113fce3278fe5
mrGreedySoap <107418049+mrGreedySoap@users.noreply.github.com>
2023-01-05 18:01:00 +0700

    feat | Изменил очередь, по одному сообщению каждому

--- new_task.py
+++ new_task.py
@@ -5,7 +5,6 @@ import pika
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
-
 message = '...' + ''.join(sys.argv[1:])
 print(' '.join(sys.argv[1:]))
 channel.basic_publish(exchange='',
